0d8203375ab5dd294a0a25d73aefb17cc78253f9
kleinlevente <kleinl@us.ibm.com>
2024-10-02 18:10:19 -0400

    Updated files

--- tests/test_models.py
+++ tests/test_models.py
@@ -175,7 +175,5 @@ class TestAccount(unittest.TestCase):
         """It should not Deserialize an account with a TypeError"""
         account = Account()
         self.assertRaises(DataValidationError, account.deserialize, [])
-        def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+        
+    
